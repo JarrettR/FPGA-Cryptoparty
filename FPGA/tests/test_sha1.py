8d12749a5a5fef224c16ac531dd6628c31d5c701
JarrettR <jrainier@gmail.com>
2016-06-20 11:17:19 -0700

    GHDL makefile fixes merged into Cocotb master, temp fix removed

--- FPGA/tests/test_sha1.py
+++ FPGA/tests/test_sha1.py
@@ -1,189 +1,124 @@
-'''
-Copyright (c) 2013 Potential Ventures Ltd
-Copyright (c) 2013 SolarFlare Communications Inc
-Copyright (c) 2016 Jarrett Rainier
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of Potential Ventures Ltd,
-      SolarFlare Communications Inc nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL POTENTIAL VENTURES LTD BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. '''
-
-import random
-import logging
 
 import cocotb
-
-from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge, ReadOnly
-from cocotb.drivers import BitDriver
-from cocotb.drivers.avalon import AvalonSTPkts as AvalonSTDriver
-from cocotb.drivers.avalon import AvalonMaster
-from cocotb.monitors.avalon import AvalonSTPkts as AvalonSTMonitor
-from cocotb.regression import TestFactory
-from cocotb.scoreboard import Scoreboard
 from cocotb.result import TestFailure
+from cocotb.monitors import BusMonitor
+from cocotb.scoreboard import Scoreboard
 
-# Data generators
-from cocotb.generators.byte import random_data, get_bytes
-from cocotb.generators.bit import (wave, intermittent_single_cycles,
-                                   random_50_percent)
+import random
 
-
-@cocotb.coroutine
-def stream_out_config_setter(dut, stream_out, stream_in):
-    """Coroutine to monitor the DUT configuration at the start
-       of each packet transfer and set the endianness of the
-       output stream accordingly"""
-    edge = RisingEdge(dut.stream_in_startofpacket)
-    ro = ReadOnly()
-    while True:
-        yield edge
-        yield ro
-        if dut.byteswapping.value:
-            stream_out.config['firstSymbolInHighOrderBits'] = \
-                not stream_in.config['firstSymbolInHighOrderBits']
-        else:
-            stream_out.config['firstSymbolInHighOrderBits'] = \
-                stream_in.config['firstSymbolInHighOrderBits']
+clock_period = 100
 
 
-class SHA1TB(object):
-
-    def __init__(self, dut, debug=False):
-    
-        self.dut = dut
-        self.stream_in  = AvalonSTDriver(dut, "avalon_st_sink", dut.clk)
-        self.stream_out = AvalonSTMonitor(dut, "avalon_st_source", dut.clk)
-        self.csr = AvalonMaster(dut, "csr", dut.clk)
-
-        self.expected_output = []
-        self.scoreboard = Scoreboard(dut)
-        self.scoreboard.add_interface(self.stream_out, self.expected_output)
-
-        # Reconstruct the input transactions from the pins and send them to our 'model'
-        self.stream_in_recovered = AvalonSTMonitor(dut, "avalon_st_sink", dut.clk, callback=self.model)
-
-
-        # Set verbosity on our various interfaces
-        level = logging.DEBUG if debug else logging.WARNING
-        self.stream_in.log.setLevel(level)
-        self.stream_in_recovered.log.setLevel(level)
-
-    def model(self, transaction):
-        """Model the DUT based on the input transaction"""
-        self.expected_output.append(transaction)
-        self.pkts_sent += 1
+class StreamBusMonitor(BusMonitor):
+    """
+    streaming bus monitor
+    """
+    _signals = ["valid", "data"]
 
     @cocotb.coroutine
-    def reset(self, duration=10000):
-        self.dut.log.debug("startofpacket DUT")
-        self.stream_in.bus.startofpacket <= 0
-        yield Timer(duration)
-        yield RisingEdge(self.dut.clk)
-        self.dut.startofpacket <= 0
-        self.dut.log.debug("Out of startofpacket")
+    def _monitor_recv(self):
+        """Watch the pins and reconstruct transactions"""
+
+        while True:
+            yield RisingEdge(self.clock)
+            yield ReadOnly()
+            if self.bus.valid.value:
+                self._recv(int(self.bus.data.value))
 
 
 @cocotb.coroutine
-def clock_gen(signal):
+def clock_gen(signal, period=10000):
     while True:
         signal <= 0
-        yield Timer(5000)
+        yield Timer(period/2)
         signal <= 1
-        yield Timer(5000)
+        yield Timer(period/2)
 
 
 @cocotb.coroutine
-def run_test(dut, data_in=None, config_coroutine=None, idle_inserter=None,
-             backpressure_inserter=None):
+def value_test(dut, num):
+    """ Test n*num/n = num """
 
-    cocotb.fork(clock_gen(dut.clk))
+    data_width = int(dut.DATA_WIDTH.value)
+    bus_width = int(dut.BUS_WIDTH.value)
+    dut.log.info('Detected DATA_WIDTH = %d, BUS_WIDTH = %d' %
+                 (data_width, bus_width))
+
+    cocotb.fork(clock_gen(dut.clk, period=clock_period))
+
+    dut.rst <= 1
+    for i in range(bus_width):
+        dut.i_data[i] <= 0
+    dut.i_valid <= 0
     yield RisingEdge(dut.clk)
-    tb = SHA1TB(dut)
+    yield RisingEdge(dut.clk)
+    dut.rst <= 0
 
-    yield tb.reset()
-    dut.stream_out_ready <= 1
+    for i in range(bus_width):
+        dut.i_data[i] <= num
+    dut.i_valid <= 1
+    yield RisingEdge(dut.clk)
+    dut.i_valid <= 0
+    yield RisingEdge(dut.clk)
+    got = int(dut.o_data.value)
 
-    # Start off any optional coroutines
-    if config_coroutine is not None:
-        cocotb.fork(config_coroutine(tb.csr))
-
-    # Send in the packets
-    for transaction in data_in():
-        yield tb.stream_in.send(transaction)
-
-    # Wait at least 2 cycles where output ready is low before ending the test
-    for i in range(2):
-        yield RisingEdge(dut.clk)
-        while not dut.stream_out_ready.value:
-            yield RisingEdge(dut.clk)
-
-    pkt_count = yield tb.csr.read(1)
-
-    if pkt_count.integer != tb.pkts_sent:
-        raise TestFailure("DUT recorded %d packets but tb counted %d" % (
-                          pkt_count.integer, tb.pkts_sent))
-    else:
-        dut.log.info("DUT correctly counted %d packets" % pkt_count.integer)
-
-    raise tb.scoreboard.result
-
-
-def random_packet_sizes(min_size=1, max_size=150, npackets=10):
-    """random string data of a random length"""
-    for i in range(npackets):
-        yield get_bytes(random.randint(min_size, max_size), random_data())
-
-
-@cocotb.coroutine
-def randomly_switch_config(csr):
-    """Twiddle the byteswapping config register"""
-    while True:
-        yield csr.write(0, random.randint(0, 1))
-
-
-factory = TestFactory(run_test)
-factory.add_option("data_in",
-                   [random_packet_sizes])
-factory.add_option("config_coroutine",
-                   [None, randomly_switch_config])
-factory.generate_tests()
-
-import cocotb.wavedrom
+    if got != num:
+        raise TestFailure(
+            'Mismatch detected: got %d, exp %d!' % (got, num))
 
 
 @cocotb.test()
-def wavedrom_test(dut):
-    """
-    Generate a JSON wavedrom diagram of a trace
-    """
-    cocotb.fork(clock_gen(dut.clk))
-    yield RisingEdge(dut.clk)
-    tb = SHA1TB(dut, True)
-    yield tb.reset()
+def mean_basic_test(dut):
+    """ Test n*5/n = 5 """
+    yield value_test(dut, 5)
 
-    with cocotb.wavedrom.trace(dut.reset_n, tb.csr.bus, clk=dut.clk) as waves:
+
+@cocotb.test()
+def mean_overflow_test(dut):
+    """ Test for overflow n*max_val/n = max_val """
+    data_width = int(dut.DATA_WIDTH.value)
+    yield value_test(dut, 2**data_width - 1)
+
+
+@cocotb.test()
+def mean_randomised_test(dut):
+    """ Test mean of random numbers multiple times """
+
+    # dut_in = StreamBusMonitor(dut, "i", dut.clk)  # this doesn't work:
+    # VPI Error vpi_get_value():
+    # ERROR - Cannot get a value for an object of type vpiArrayVar.
+
+    dut_out = StreamBusMonitor(dut, "o", dut.clk)
+
+    exp_out = []
+    scoreboard = Scoreboard(dut)
+    scoreboard.add_interface(dut_out, exp_out)
+
+    data_width = int(dut.DATA_WIDTH.value)
+    bus_width = int(dut.BUS_WIDTH.value)
+    dut.log.info('Detected DATA_WIDTH = %d, BUS_WIDTH = %d' %
+                 (data_width, bus_width))
+
+    cocotb.fork(clock_gen(dut.clk, period=clock_period))
+
+    dut.rst <= 1
+    for i in range(bus_width):
+        dut.i_data[i] = 0
+    dut.i_valid <= 0
+    yield RisingEdge(dut.clk)
+    yield RisingEdge(dut.clk)
+    dut.rst <= 0
+
+    for j in range(10):
+        nums = []
+        for i in range(bus_width):
+            x = random.randint(0, 2**data_width - 1)
+            dut.i_data[i] = x
+            nums.append(x)
+        dut.i_valid <= 1
+
+        nums_mean = sum(nums) // bus_width
+        exp_out.append(nums_mean)
         yield RisingEdge(dut.clk)
-        yield tb.csr.read(0)
-        yield RisingEdge(dut.clk)
-        yield RisingEdge(dut.clk)
-        dut.log.info(waves.dumpj())
+        dut.i_valid <= 0
