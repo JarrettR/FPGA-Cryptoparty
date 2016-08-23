a5f2aca7fd37db3dbd63168cd65254535386fb89
JarrettR <jrainier@gmail.com>
2016-08-23 12:01:58 -0700

    Added licenses

--- FPGA/tests/test_hmac.py
+++ FPGA/tests/test_hmac.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+################################################################################
+#  Top level regresssion tests for HMAC portion - Modify Makefile appropriately 
+#    Copyright (C) 2016  Jarrett Rainier
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge, FallingEdge
