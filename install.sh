ef2f76bb6359e72d0b73c6089322010e10196e4f
JarrettR <jrainier@gmail.com>
2016-10-27 14:57:48 -0700

    Added wpa2slow as a tool

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 git submodule update --init
 apt-get update
+apt-get install -y pip
+cd tools/wpa2slow && (pip install -e .) && cd ../..
 apt-get install -y gnat
 cd tools/ghdl && (./configure --prefix=/usr/local) && make && make install && cd ../..
